d304b814b5ed76b8f9149c727342793d2c313989
Amen Ramadan <ramadanamen407@gmail.com>
2025-04-09 00:53:05 +0300

    fix center register and login page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="ml-auto">
+    <div className="mx-auto flex items-center justify-center">
       <SignIn />
     </div>
   );
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="mx-auto flex items-center justify-center">
+      <SignUp />
+    </div>
+  );
 }
