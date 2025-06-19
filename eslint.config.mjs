ca6aa145877e3cbcc7358e3150b4e6dacef53912
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-19 02:00:45 +0000

    100%

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,55 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific TypeScript rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      // Optional: disable the unescaped entities rule
+      "react/no-unescaped-entities": "off",
+      "no-unused-vars": "off",
+      // Disable the no-console rule
+      "no-console": "off",
+      // Disable the no-debugger rule
+      "no-debugger": "off",
+      // Disable the no-undef rule        
+      "no-undef": "off",
+      // Disable the no-restricted-syntax rule
+      "no-restricted-syntax": "off",
+      // Disable the no-constant-condition rule
+      "no-constant-condition": "off",
+      // Disable the no-duplicate-imports rule          
+      "no-duplicate-imports": "off",
+      // Disable the no-shadow rule
+      "no-shadow": "off",
+      // Disable the no-unused-expressions rule
+      "no-unused-expressions": "off",
+      // Disable the no-use-before-define rule          
+      "no-use-before-define": "off",
+      // Disable the no-var rule
+      "no-var": "off",
+      // Disable the prefer-const rule
+      "prefer-const": "off",
+      // Disable the prefer-rest-params rule
+      "prefer-rest-params": "off",              
+      // allowObjectTypes
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "ForInStatement",
+          message: "for...in loops are not allowed. Use Object.keys() or Object.entries() instead.",
+        },
+        {
+          selector: "LabeledStatement",
+          message: "Labeled statements are not allowed.",
+        },
+      ],
+      // Disable the no-unsafe-optional-chaining rule
+      "no-unsafe-optional-chaining": "off",
+
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "10mb",
