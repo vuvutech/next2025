66cb10e3dfb42c19ea7ed45f5e2b7be7dab6af4d
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-25 05:55:49 +0000

    minor changes

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
   return (
        <div className='block p-2 '>
               <Toaster />
-              <Jumbotron heroImage='/images/chess.jpg' />
+              <Jumbotron heroImage='/images/about.jpg' />
             <div className='md:p-4 max-w-8xl mx-auto space-y-4'>
                 {children}
             </div>
