bbdadcb3e658ab1366a1b78ee30a0f9369130135
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-27 12:32:55 +0000

    ui updates ...

--- components/ui/CTA.tsx
+++ components/ui/CTA.tsx
@@ -13,14 +13,15 @@ const CTA = () => {
         <div className="grid grid-cols-3 md:h-[50dvh]  w-full flex-col gap-16 overflow-hidden rounded-xl bg-accent p-4 sm:p-8 md:rounded-xl lg:flex-row lg:items-center lg:p-16">
           <div className="col-span-full md:col-span-2 relative ">
             <h1 className=" text-xl sm:text-2xl  md:mb-4 md:text-3xl lg:mb-6">
-              Stay in the loop and never miss out on what's happening!
+              STAY AHEAD OF THE CURVE
             </h1>
             <Separator className="my-2 max-w-1/5 bg-primary " />
 
             <p className="text-foreground lg:text-lg">
-              Be the first to hear about our latest and most exciting events,
-              special announcements, important updates, and exclusive
-              notifications—all sent directly to you.
+              Stay in the loop and never miss out on what's happening! Be the
+              first to hear about our latest and most exciting events, special
+              announcements, important updates, and exclusive notifications—all
+              sent directly to you.
             </p>
           </div>
           <div className="col-span-full md:col-span-1 ">
