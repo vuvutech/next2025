110f57639fd7640bfc1925206abafb9d173b9dcb
Pius Opoku-Fofie <pius1919@gmail.com>
2025-07-21 23:56:41 +0000

    good updates

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { BanConfirmationDialog } from "./users/BanConfirmationDialog";
 
 
 export default function AdminLayout({
