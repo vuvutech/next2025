05bc655987e093c5dd015f0acdefa720ef32c50c
Pius Opoku-Fofie <pius1919@gmail.com>
2025-07-22 00:11:42 +0000

    good updates

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/prisma/dbConnect";
 import ClientWrapper from "./ClientWrapper"; // your table wrapper
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminUsersPage() {
   const users = await prisma.user.findMany({
     orderBy: {
