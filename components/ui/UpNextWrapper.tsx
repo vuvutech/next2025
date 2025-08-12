b9379af60195a12034a770ab5adffa69a9ffb76f
Pius Opoku-Fofie <pius1919@gmail.com>
2025-08-12 19:24:43 +0000

    upnext

--- components/ui/UpNextWrapper.tsx
+++ components/ui/UpNextWrapper.tsx
@@ -4,6 +4,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import { oswald } from "@/config/fonts";
 
 type NextEdition = {
   title: string;
@@ -46,7 +47,7 @@ export default function UpNextWrapper() {
     <div className="bg-linear-to-r from-primary to-primary/90">
       <div className="max-w-[85rem] px-2 sm:px-6 lg:px-8 mx-auto">
         <div className="py-1 flex  justify-center items-center ">
-          <p className=" inline-block  text-white capitalize  text-[10px] md:text-xs ">
+          <p className={`${oswald.className} text-white uppercase leading-[10px] text-[10px] md:text-sm `}>
             <span className="font-bold uppercase">UpNext:</span>
             &nbsp; <span className="font-mono">{nextEdition.title}</span>
             &nbsp;|&nbsp;
@@ -70,10 +71,10 @@ export default function UpNextWrapper() {
           </p>
           &nbsp;&nbsp;
             <Link
-              className="py-1 px-2.5 inline-flex justify-center items-center gap-x-1 bg-white/10 text-[10px] font-semibold text-white hover:bg-white/20 focus:outline-hidden focus:bg-white/20 rounded-full"
+              className="py-0.5 sm:py-1 px-2.5 inline-flex justify-center items-center gap-x-1 bg-white/10 text-[10px] md:text-sm uppercase font-semibold text-white hover:bg-white/20 focus:outline-hidden focus:bg-white/20 rounded-full"
               href={`/apply`}
             >
-              Apply
+              Apply Now
               <svg
                 className="shrink-0 size-3.5"
                 xmlns="http://www.w3.org/2000/svg"
