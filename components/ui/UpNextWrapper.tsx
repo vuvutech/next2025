79f6466cdbb6046d2d5a6c84d13968472012907a
Pius Opoku-Fofie <pius1919@gmail.com>
2025-08-18 11:11:44 +0000

    upnext updates

--- components/ui/UpNextWrapper.tsx
+++ components/ui/UpNextWrapper.tsx
@@ -5,6 +5,14 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { oswald } from "@/config/fonts";
+import {
+  LucideArrowRight,
+  LucideEllipsisVertical,
+  LucideMoveRight,
+  LucideTally1,
+  SeparatorVertical,
+} from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 type NextEdition = {
   title: string;
@@ -45,12 +53,15 @@ export default function UpNextWrapper() {
 
   return (
     <div className="bg-linear-to-r from-primary to-primary/90">
-      <div className="max-w-[85rem] px-2 sm:px-6 lg:px-8 mx-auto">
-        <div className="py-1 flex  justify-center items-center ">
-          <p className={`${oswald.className} text-white uppercase leading-[10px] text-[10px] md:text-sm `}>
+      <div className="max-w-[85rem] px-1 py-1  sm:px-6 lg:px-8 mx-auto">
+        <div className="flex  justify-center items-center ">
+          <p
+            className={`${oswald.className} text-white uppercase leading-[10px] text-[10px] md:text-[12px] flex items-center `}
+          >
             <span className="font-bold uppercase">UpNext:</span>
             &nbsp; <span className="font-mono">{nextEdition.title}</span>
-            &nbsp;|&nbsp;
+
+            <LucideTally1 size={14} className="pl-2  box-content" />
             <span className="font-semibold uppercase ">
               {nextEdition.startDate
                 ? new Date(nextEdition.startDate).toLocaleDateString("en-US", {
@@ -67,29 +78,18 @@ export default function UpNextWrapper() {
                   })
                 : ""}{" "}
             </span>
-            
           </p>
           &nbsp;&nbsp;
-            <Link
-              className="py-0.5 sm:py-1 px-2.5 inline-flex justify-center items-center gap-x-1 bg-white/10 text-[10px] md:text-sm uppercase font-semibold text-white hover:bg-white/20 focus:outline-hidden focus:bg-white/20 rounded-full"
-              href={`/apply`}
-            >
-              Apply Now
-              <svg
-                className="shrink-0 size-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="m9 18 6-6-6-6" />
-              </svg>
-            </Link>
+          <Link
+            className="py-0.5 sm:py-1 px-3 md:px-4 inline-flex justify-center items-center gap-x-1 bg-white/10 text-[9px] md:text-[11px] font-oswald
+                uppercase font-semibold text-white hover:bg-white/20 focus:outline-hidden focus:bg-white/20 rounded-full"
+            href={`/apply`}
+          >
+            Apply Now
+            <span className=" sm:inline-flex">
+              <LucideMoveRight size={18} />
+            </span>
+          </Link>
         </div>
       </div>
     </div>
