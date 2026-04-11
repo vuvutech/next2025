b49d1a51eb57351c0469b470ad6a126b21ac2310
Pius Kwame Opoku-Fofie <pius1919@gmail.com>
2026-04-11 18:01:29 +0000

    better-auth issues fixed.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,48 +1,53 @@
 // lib/auth-client.ts
 import { createAuthClient } from "better-auth/react";
 import {
-  organizationClient,
-  passkeyClient,
-  twoFactorClient,
-  adminClient,
-  multiSessionClient,
-  oneTapClient,
-  oidcClient,
-  genericOAuthClient,
-  emailOTPClient,
+	organizationClient,
+	twoFactorClient,
+	adminClient,
+	multiSessionClient,
+	oneTapClient,
+	oidcClient,
+	genericOAuthClient,
+	emailOTPClient,
 } from "better-auth/client/plugins";
 import { toast } from "sonner";
 import { baseUrl } from "./metadata";
+import { ac, adminRole, superAdminRole } from "./auth/permissions";
 
 export const client = createAuthClient({
-  baseURL: `${baseUrl}` || "http://localhost:3000",
-  plugins: [
-    emailOTPClient(),
-    organizationClient(),
-    twoFactorClient({
-      onTwoFactorRedirect() {
-        window.location.href = "/auth/two-factor";
-      },
-    }),
-    passkeyClient(),
-    oneTapClient({
-      clientId: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    adminClient(),
-    multiSessionClient(),
-    oneTapClient({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-    }),
-    oidcClient(),
-    genericOAuthClient(),
-  ],
-  fetchOptions: {
-    onError(e) {
-      if (e.error.status === 429) {
-        toast.error("Too many requests. Please try again later.");
-      }
-    },
-  },
+	baseURL: `${baseUrl}` || "http://localhost:3000",
+	plugins: [
+		emailOTPClient(),
+		organizationClient(),
+		twoFactorClient({
+			onTwoFactorRedirect() {
+				window.location.href = "/auth/two-factor";
+			},
+		}),
+		oneTapClient({
+			clientId: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		adminClient({
+			ac,
+			roles: {
+				ADMIN: adminRole,
+				SUPERADMIN: superAdminRole,
+			},
+		}),
+		multiSessionClient(),
+		oneTapClient({
+			clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+		}),
+		oidcClient(),
+		genericOAuthClient(),
+	],
+	fetchOptions: {
+		onError(e) {
+			if (e.error.status === 429) {
+				toast.error("Too many requests. Please try again later.");
+			}
+		},
+	},
 });
 
 export const {
